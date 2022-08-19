225059f428bfa0157dabfc9f516f13d6323a53a8
test <test@email.com>
2022-08-19 16:57:18 +0600

    Added file conftest.py

---
+++ Stepik_lessons/conftest1/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+#
+# def pytest_addoption(parser):
+#     parser.addoption('--browser_name', action='store', default='chrome',
+#                      help="Choose browser: chrome or firefox")
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='None',
+                     help="Choose language: ru, en ...")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    # browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    # if user_language == "ru":
+    #     browser = webdriver.Chrome()
+    # elif browser_name == "firefox":
+    #     browser = webdriver.Firefox()
+    # else:
+    #     raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
